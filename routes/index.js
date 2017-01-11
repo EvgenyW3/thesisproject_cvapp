3f2849a9ec65951be545e7d86bfd45426ba1e516
EvgenyW3 <evgeny0191@gmail.com>
2017-01-11 15:34:27 +0200

    Delete for schoold works on backend as well

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var Page = require('../models/page.js');
 var mongoose = require('mongoose');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
 	Page.findOne(function(err, page){
 		if(err) throw err;
 		if(!page){
@@ -12,6 +12,7 @@ router.get('/', function(req, res, next) {
             var page = new Page();
             page.save();
 		}else{
+		    console.log(page.education);
 			var list = page.skills;
 			var size = 0;
             for(var i = 0; i < list.length; i++){
@@ -22,11 +23,22 @@ router.get('/', function(req, res, next) {
 	});
 });
 
-router.post('/update', function(req, res, next) {
-	Page.update({ _id: req.body.id}, req.body, function(err, page){
-		if(err) throw err;
-		res.redirect('/');
-	});
+router.post('/update', function(req, res) {
+    Page.update({_id: req.body.id}, req.body, function (err) {
+        if (err) throw err;
+        res.redirect('/');
+    });
+});
+router.get('/delete/school/:id', function (req, res) {
+    Page.findOne(function (err, page) {
+        console.log("This is output"+page.education.id);
+        if(err) throw err;
+        var job = page.education.id(req.params.id).remove();
+        page.save(function (error) {
+            if(error) throw error;
+                res.redirect('/');
+            });
+    });
 });
 
 
